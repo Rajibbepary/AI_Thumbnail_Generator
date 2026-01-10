3e3ee98dbf5ba1420e965359ba2a4c75e19d2132
Rajibbepary <rajib635356@gmail.com>
2026-01-10 21:28:11 +0600

    generate page ui update

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -22,9 +22,26 @@ const Generate = () => {
                    <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
                     <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
                         <div>
-                            <h2>Create Your Thumbnail</h2>
-                            <p>Describe your vision and let AI bring it to life</p>
+                            <h2 className="text-xl font-bold text-zinc-100 mb-1">Create Your Thumbnail</h2>
+                            <p className="text-sm text-zinc-400">Describe your vision and let AI bring it to life</p>
                         </div>
+                        <div className="space-y-5">
+                            {/* TITEL INPUT */}
+                            <div className="space-y-2">
+                                <label className="block text-sm font-medium">Title or Topic</label>
+                                <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} maxLength={100} placeholder="e.g., 10 Tips for Better Sleep" className="w-full px-4 py-3 rounded-lg border border-white/15 bg-black/20 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500"/>
+                                <div className="flex justify-end"> <span className="text-xs text-zinc-400">{title.length}/100</span></div>
+                            </div>
+                             {/* { AspectRationSelector} */}
+                             {/* StyleSelector */}
+                             {/* ColorSchemeSelector */}
+                        </div>
+                      
+
+                        {/* BUTTON */}
+                        {!id && (
+                            <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">{loading ? 'Generating...': 'Generate Thumbnail'}</button>
+                        )}
                     </div>
                    </div>
                    {/* RIGHT PANEL */}
