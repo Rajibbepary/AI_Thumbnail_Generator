1b42ba9a78f997392c87ae9f708a8ca45550a26b
Rajibbepary <rajib635356@gmail.com>
2026-01-14 09:24:38 +0600

    fix: add button type and correct conditional className

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -5,6 +5,7 @@ import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } 
 import SoftBackdrop from "../components/SoftBackdrop";
 import AspectRatioSelector from "../components/AspectRatioSelector";
 import StyleSelector from "../components/StyleSelector";
+import ColorSchemeSelector from "../components/ColorSchemeSelector";
 
 
 const Generate = () => {
@@ -45,7 +46,7 @@ const Generate = () => {
                              <StyleSelector value={style} onChange={setStyle} isOpen={styleDropdownOpen} setIsOpen={setStyleDropdownOpen}/>
 
                              {/* ColorSchemeSelector */}
-
+                                <ColorSchemeSelector value={colorSchemeId} onChange={setColorSchemeId}/>
                              {/* DETALS */}
                              <div className="space-y-2">
                                 <label className="block text-sm font-medium">Additional Prompts <span className="text-zinc-400 text-xs">(optional)</span></label>
