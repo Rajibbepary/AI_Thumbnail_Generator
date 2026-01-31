e24aae7c569d378cfbbeb708a0e026e7dbac115e
Rajibbepary <rajib635356@gmail.com>
2026-01-31 09:57:14 +0600

    fix: thumbnail generation response and buffer handling

--- server/controllers/ThumbnailControllers.ts
+++ server/controllers/ThumbnailControllers.ts
@@ -2,6 +2,10 @@
 import { Request, Response } from "express";
 import Thumbnail from "../Models/Thumblify.js";
 import { GenerateContentConfig, HarmBlockThreshold, HarmCategory } from "@google/genai";
+import ai from "../configs/ai.js";
+import path from "path";
+import fs from "fs";
+import {v2 as cloudinary} from 'cloudinary'
 
 
 const stylePrompts = {
@@ -25,7 +29,7 @@ const colorSchemeDescriptions = {
 
 
 
-export const generateThumbnail = async (req: Request, rest: Response)=>{
+export const generateThumbnail = async (req: Request, res: Response)=>{
     try{
         const {userId} = req.session;
         const {title, prompt: user_prompt, style, aspect_ratio,
@@ -62,9 +66,61 @@ const generationConfig: GenerateContentConfig = {
     ]
 }
 
-let prompt = `Create a ${stylePrompts} for: "${title}"`
+let prompt = `Create a ${stylePrompts[style as keyof stylePrompts]} for: "${title}"`;
+if(color_scheme){
+    prompt += `Use a ${colorSchemeDescriptions[color_scheme as keyof typeof colorSchemeDescriptions]} color scheme.`
+}
 
-    } catch (error){
+if(user_prompt){
+    prompt += `Additional details: ${user_prompt}.`
+}
 
+prompt += `The thumbnail should be ${aspect_ratio}, visually stunning, and designed to maximize click-through rate. Make it bold, professional, and impossible to ignore.`
+
+//Generate the image using the ai model
+const response: any = await ai.models.generateContent({
+    model,
+    contents: [prompt],
+    config: generationConfig
+})
+
+//check if the response is valid
+if(!response?.candidates?.[0]?.content?.parts){
+    throw new Error('Unexpected response')
+}
+
+const parts = response.candidates[0].content.parts
+
+let finalBuffer: Buffer | null = null;
+
+for(const part of parts){
+    if(part.inlineData){
+        finalBuffer = Buffer.from(part.inlineData.data, 'base64')
+    }
+}
+
+const filename = `final-output-${Date.now()}.png`;
+const filePath = path.join('images', filename)
+
+//Create the images directory if it doesn't exist
+fs.mkdirSync('images', {recursive: true})
+
+//write the final image to the file
+fs.writeFileSync(filePath, finalBuffer!);
+
+const UploadResult = await cloudinary.uploader.upload(filePath, {resource_type: 'image'})
+
+thumbail.image_url = UploadResult.url;
+thumbail.isGenerating = false;
+await thumbail.save()
+
+res.json({message: 'Thumbnail Generated', thumbail})
+
+// remove image file from disk
+fs.unlinkSync(filePath)
+
+    } catch (error: any){
+        console.log(error);
+        res.status(500).json({ message: error.message});
     }
 }
